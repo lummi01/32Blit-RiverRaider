1418caabcb84623e843a4157c81ba8d077ed4cb2
Lummi <72337143+lummi01@users.noreply.github.com>
2025-08-06 09:56:01 +0200

    Delete assets directory

--- assets/riverraider.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.0" name="riverraider" tilewidth="8" tileheight="8" tilecount="256" columns="32">
- <image source="riverraider.png" width="128" height="128"/>
-</tileset>
